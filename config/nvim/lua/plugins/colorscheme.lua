18185fd80f0e86c8993cee5a8790df83986f3022
Matej Konrad <konrad.matej@gmail.com>
2026-05-17 23:41:12 +0200

    Switch to gruvbox

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -222,10 +222,25 @@ return {
   },
   { "EdenEast/nightfox.nvim", enabled = false },
   {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.gruvbox_material_background = "medium"
+      vim.g.gruvbox_material_foreground = "material"
+      vim.g.gruvbox_material_better_performance = 1
+      vim.g.gruvbox_material_enable_italic = 1
+      vim.g.gruvbox_material_diagnostic_text_highlight = 1
+      vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
+      vim.g.gruvbox_material_float_style = "dim"
+    end,
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       -- colorscheme = "lackluster-hack",
-      colorscheme = "vesper",
+      -- colorscheme = "vesper",
+      colorscheme = "gruvbox-material",
     },
   },
 }
