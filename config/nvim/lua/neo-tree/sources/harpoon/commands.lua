de1ce6b58d42c414826cda64338b5d024d1a71ac
Matej Konrad <matej@framer.com>
2026-06-23 14:47:29 +0200

    Remove neotree

--- config/nvim/lua/neo-tree/sources/harpoon/commands.lua
+++
@@ -1,2 +0,0 @@
--- Reuse neo-tree's shared commands (open, close_node, refresh, etc.).
-return vim.tbl_deep_extend("force", {}, require("neo-tree.sources.common.commands"))
--- config/nvim/lua/neo-tree/sources/harpoon/components.lua
+++
@@ -1,2 +0,0 @@
--- Reuse neo-tree's shared components (icon, name, git_status, etc.).
-return vim.tbl_deep_extend("force", {}, require("neo-tree.sources.common.components"))
