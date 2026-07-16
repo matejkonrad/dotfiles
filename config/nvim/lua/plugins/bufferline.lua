7e596d22ef1f0317317a010726fbb328002593b0
Matej Konrad <matej@framer.com>
2026-07-16 11:21:47 +0200

    doom

---
+++ config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,10 @@
+return {
+  "akinsho/bufferline.nvim",
+  opts = {
+    options = {
+      -- Show the buffer line as soon as there's one buffer. LazyVim defaults
+      -- this to false, which hides the line until you have 2+ buffers.
+      always_show_bufferline = true,
+    },
+  },
+}
